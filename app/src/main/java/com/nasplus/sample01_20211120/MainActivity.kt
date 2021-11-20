93d2ea58cb9728758f9e680bf3f1ffb237f310bb
sofcom <88303053+sofcom@users.noreply.github.com>
2021-11-21 00:45:15 +0900

    Initial commit

---
+++ app/src/main/java/com/nasplus/sample01_20211120/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.nasplus.sample01_20211120
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("onCreate()에서", "로그 출력")
+
+        
+
+    }
+}
